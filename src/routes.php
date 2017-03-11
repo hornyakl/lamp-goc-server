15a433b13322599a40c7c0e4e8e24a9f2385237a
hornyakl <hornyakl@gmail.com>
2017-03-11 15:27:15 +0100

    add create quest route and view file

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,14 @@
 <?php
 // Routes
 
+$app->get('/create-quest', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'create-quest.phtml', $args);
+});
+
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
