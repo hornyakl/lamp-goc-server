6eca58fc8995212d505e3355b26f63947abc20ba
agocs <agocsp@gmail.com>
2017-03-11 18:37:43 +0100

    quest accepted template and route

--- src/routes.php
+++ src/routes.php
@@ -1,17 +1,21 @@
 <?php
 // Routes
 
-$app->get('/quest-list', function ($request, $response, $args) {
-    $quest = $this->get('quests');
-    return $this->response->withJSON($quest);
-});
-
 $app->get('/quest-create', function ($request, $response, $args) {
     return $this->renderer->render($response, 'quest-create.phtml', $args);
 });
 
 $app->get('/quest-saved', function ($request, $response, $args) {
     return $this->renderer->render($response, 'quest-saved.phtml', $args);
+});
+
+$app->get('/quest-list', function ($request, $response, $args) {
+    $quest = $this->get('quests');
+    return $this->response->withJSON($quest);
+});
+
+$app->get('/quest-accepted', function ($request, $response, $args) {
+    return $this->renderer->render($response, 'quest-accepted.phtml', $args);
 });
 
 $app->get('/[{name}]', function ($request, $response, $args) {
